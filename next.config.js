487b66641ed7a33bb8441284bf2b946410475c80
Tamate <tamate.tolo@gmail.com>
2024-08-13 16:55:29 +0100

    config setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/pairs',
+  output: 'export',
   reactStrictMode: true,
-  swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
